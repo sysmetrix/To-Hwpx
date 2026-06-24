777c266b3f7842de427de2032d51c1edc67ea72a
sysmetrix <seowonha@gmail.com>
2026-06-24 16:48:38 +0900

    fix: 마크다운 변환 시 작은따옴표가 이스케이프되는 문제 수정

--- js/hwpx.js
+++ js/hwpx.js
@@ -143,8 +143,7 @@ function xmlEsc(s) {
         .replace(/&/g, '&amp;')
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&apos;');
+        .replace(/"/g, '&quot;');
 }
 
 /**
